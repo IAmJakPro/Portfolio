8ed3450ddc44b50fd564fa66eae6a6e4a304c688
IAmJakPro <jakdev20@gmail.com>
2021-12-16 20:50:40 +0100

    Added build file

---
+++ build/precache-manifest.1c6456dfedae450e5b323505f81ad0ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f6116d25e9a4bef8d106e97c575e470",
+    "url": "/index.html"
+  },
+  {
+    "revision": "343b42e78bd63b6c3e2c",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "58e89acd406ebee9bae8",
+    "url": "/static/css/main.8b643ff1.chunk.css"
+  },
+  {
+    "revision": "343b42e78bd63b6c3e2c",
+    "url": "/static/js/2.02689371.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.02689371.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58e89acd406ebee9bae8",
+    "url": "/static/js/main.15d5e27e.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
